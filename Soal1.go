02e557d3dca1e5a1f2827c74987e86228a533408
alifnaufalyasin <alifnaufal171199@gmail.com>
2021-07-29 16:56:01 +0700

    Update Soal 1

--- Soal1.go
+++ Soal1.go
@@ -9,6 +9,8 @@ func main() {
 	fmt.Print("Masukkan Angka : ")
 	var angka int
 	fmt.Scanln(&angka)
+
+	// Mod dapat menggunakan math.Mod atau %
 	if math.Mod(float64(angka), 3) == 0 && math.Mod(float64(angka), 5) == 0 {
 		fmt.Println("Hello World")
 	} else if math.Mod(float64(angka), 5) == 0 {
