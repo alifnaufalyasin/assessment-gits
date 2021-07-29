134f5d41a493c5680f8e4fa181b81a0cb62f97b7
alifnaufalyasin <alifnaufal171199@gmail.com>
2021-07-29 10:27:49 +0700

    Add Soal 1

---
+++ Soal1.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Print("Masukkan Angka : ")
+	var angka int
+	fmt.Scanln(&angka)
+	if math.Mod(float64(angka), 3) == 0 && math.Mod(float64(angka), 5) == 0 {
+		fmt.Println("Hello World")
+	} else if math.Mod(float64(angka), 5) == 0 {
+		fmt.Println("World")
+	} else if math.Mod(float64(angka), 3) == 0 {
+		fmt.Println("Hello")
+	}
+}
